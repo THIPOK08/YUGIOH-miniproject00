c2adce132c28c364233a25e583aaa7657a0927c7
THIPOK08 <std.67122420209@ubru.ac.th>
2026-03-08 10:11:14 +0700

    04

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from yugioh import create_app
+from yugioh.extensions import db  # เพิ่มบรรทัดนี้เพื่อดึงตัวจัดการฐานข้อมูลมาใช้
 
 app = create_app()
+
+# เพิ่มส่วนนี้ลงไปครับ
+with app.app_context():
+    db.create_all()
