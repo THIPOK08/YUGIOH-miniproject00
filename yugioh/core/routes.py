cb2540e528398c87a8928274e4f7d12f5918d2a5
THIPOK08 <std.67122420209@ubru.ac.th>
2026-03-11 21:30:17 +0700

    026

--- yugioh/core/routes.py
+++ yugioh/core/routes.py
@@ -6,11 +6,18 @@ from yugioh.models import db, Card, Type
 core_bp = Blueprint('core', __name__, template_folder='templates')
 
 @core_bp.route('/')
-@login_required
 def index():
     page = request.args.get('page', 1, type=int)
-    # แก้ตรงนี้เพื่อแยกเด็คของใครของมัน
-    query = sa.select(Card).where(Card.user_id == current_user.id) 
-    cards = db.session.paginate(query, per_page=8, page=page)
+    cards = db.paginate(sa.select(Card), per_page=8, page=page)
 
-    return render_template('core/index.html', title='Home', cards=cards)
+    return render_template('core/index.html',
+                           title='Yugioh Deck',
+                           cards=cards)
+
+@core_bp.route('/card/<int:id>')
+def detail(id):
+    card = db.session.get(Card, id)
+
+    return render_template('core/yugioh_detail.html',
+                           title='Card Details',
+                           card=card)
